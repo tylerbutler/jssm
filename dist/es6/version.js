6168407eaa0a643be236de14d89df3948fd1b1c9
Tyler Butler <tylerbu@microsoft.com>
2024-05-22 16:22:29 -0700

    build

--- dist/es6/version.js
+++ dist/es6/version.js
@@ -1,2 +1,2 @@
-const version = "5.98.2", build_time = 1714068061930;
+const version = "5.98.2", build_time = 1716419613928;
 export { version, build_time };
--- src/ts/version.ts
+++ src/ts/version.ts
@@ -1,5 +1,5 @@
 
 const version    : string = "5.98.2",
-      build_time : number = 1714068061930;
+      build_time : number = 1716419613928;
 
 export { version, build_time };
